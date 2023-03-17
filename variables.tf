0cbfd7405a5c1dffe822fafcef5553f737a59bc2
Tyler Kontra <tyler@zeet.co>
2023-03-17 11:56:12 -0700

    legacy support: get self-managed node groups by tag:ZeetClusterId

--- variables.tf
+++ variables.tf
@@ -17,3 +17,9 @@ variable "sns_subscription_endpoint" {
 variable "sns_subscription_protocol" {
   default = "https"
 }
+
+variable "zeet_cluster_id" {
+  // optional
+  default = ""
+  description = "id for the zeet cluster record. optional: when non-empty, it will be used to look up self-managed ec2 node groups"
+}
